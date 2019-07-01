ca444df0c85c08e7a78543824cc6369a989faa38
Nizar <niezar.rk@gmail.com>
2019-07-01 22:19:18 +0700

    izin view lampiran

--- src/views/layanan/izin.vue
+++ src/views/layanan/izin.vue
@@ -102,15 +102,15 @@
         <b-modal id="modal-1" title="Lampiran" ref="modal">
             <b-row>
                 <b-col>
-                    <b-img thumbnail fluid :src="lampiran[0]" alt="Image 1"></b-img>
+                    <b-img thumbnail fluid :src="lampiran[0]" class="myImg" @click="modalImg(lampiran[0], 'Surat dari Kelurahan')" alt="Image 1"></b-img>
                     <small>Surat dari Kelurahan</small>
                 </b-col>
                 <b-col>
-                    <b-img thumbnail fluid :src="lampiran[1]" alt="Image 2"></b-img>
+                    <b-img thumbnail fluid :src="lampiran[1]" class="myImg" @click="modalImg(lampiran[1], 'Kartu Tanda Penduduk')" alt="Image 2"></b-img>
                     <small>Kartu Tanda Penduduk</small>
                 </b-col>
                 <b-col>
-                    <b-img thumbnail fluid :src="lampiran[2]" alt="Image 3"></b-img>
+                    <b-img thumbnail fluid :src="lampiran[2]" class="myImg" @click="modalImg(lampiran[2], 'Kartu Keluarga')" alt="Image 3"></b-img>
                     <small>Kartu Keluarga</small>
                 </b-col>
             </b-row>
@@ -128,6 +128,12 @@
                 </b-form-input>
             </b-form-group>
         </b-modal>
+        <!-- Modal Img -->
+        <div id="myModal" class="modal">
+            <span class="close">&times;</span>
+            <img class="modal-content" id="img01">
+            <div id="caption"></div>
+        </div>
    </div>
 </template>
 <script>
@@ -274,9 +280,119 @@ export default {
               this.makeToast(error.message, 'Terjadi Kesalahan', 'danger');
           }
         },
+        modalImg(img, desk) {
+            this.img = img;
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the image and insert it inside the modal - use its "alt" text as a caption
+            let modalImg = document.getElementById("img01");
+            let captionText = document.getElementById("caption");
+
+            modal.style.display = "block";
+            modalImg.src = this.img;
+            captionText.innerHTML = desk;
+            
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() { 
+                modal.style.display = "none";
+            }
+        },
     },
     mixins: [date]
     
 }
 </script>
+<style scoped>
+.myImg {
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 9999; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 500px;
+}
+
+/* Caption of Modal Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {  
+  -webkit-animation-name: zoom;
+  -webkit-animation-duration: 0.6s;
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+  from {-webkit-transform:scale(0)} 
+  to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+  from {transform:scale(0)} 
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 60px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+  .modal-content {
+    width: 100%;
+  }
+}
+</style>
+
 
